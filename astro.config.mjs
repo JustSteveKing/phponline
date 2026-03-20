8c99684bd2cc054c6b88c53f8689a0757130d28b
Steve McDougall <juststevemcd@gmail.com>
2026-03-20 13:11:39 +0000

    chore: refactoring the UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,38 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-      // Allows Astro to optimize images from these specific hosts
-      remotePatterns: [
-      { protocol: 'https', hostname: 'feed.laravel-news.com' },
-      { protocol: 'https', hostname: '**.symfony.com' },
-      { protocol: 'https', hostname: '**.php.net' },
-      { protocol: 'https', hostname: '**.gravatar.com' }, // Good for author avatars
-      ],
+    // Allows Astro to optimize images from these specific hosts
+    remotePatterns: [
+      { protocol: "https", hostname: "feed.laravel-news.com" },
+      { protocol: "https", hostname: "**.symfony.com" },
+      { protocol: "https", hostname: "**.php.net" },
+      { protocol: "https", hostname: "**.gravatar.com" }, // Good for author avatars
+      { protocol: "https", hostname: "github.com" },
+    ],
   },
 
-  site: 'https://www.phponline.dev',
+  site: "https://www.phponline.dev",
 
   vite: {
-      plugins: [tailwindcss()],
-      ssr: {
-          external: ['/pagefind/pagefind.js']
+    plugins: [tailwindcss()],
+    ssr: {
+      external: ["/pagefind/pagefind.js"],
+    },
+    optimizeDeps: {
+      exclude: ["/pagefind/pagefind.js"],
+    },
+    build: {
+      rollupOptions: {
+        external: ["/pagefind/pagefind.js"],
       },
-      optimizeDeps: {
-          exclude: ['/pagefind/pagefind.js']
-      },
-      build: {
-          rollupOptions: {
-              external: ['/pagefind/pagefind.js']
-          }
-      }
+    },
   },
 
-  integrations: [svelte(), sitemap()]
+  integrations: [svelte(), sitemap()],
 });
